9350b55f5c34fb91058aed84e98df6ce3ff7b6c8
buci <bucialphard@gmail.com>
2025-06-10 18:15:37 +0700

    integration sign in done

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/_components/form.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useFormState, useFormStatus } from "react-dom";
+import { SignIn } from "../lib/actions";
+import { ActionResult } from "@/types";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircleIcon } from "lucide-react";
+
+const initialState: ActionResult = {
+  error: "",
+};
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" className="w-full" disabled={pending}>
+      {pending ? "Loading..." : "Sign In"}
+    </Button>
+  );
+}
+
+export default function FormSignIn() {
+  const [state, formAction] = useFormState(SignIn, initialState);
+  console.log({ state });
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {state.error !== "" && (
+          <Alert variant="destructive" className="mb-4">
+            <AlertCircleIcon />
+            <AlertTitle>Unable to Sign In</AlertTitle>
+            <AlertDescription>
+              <p>{state.error}</p>
+            </AlertDescription>
+          </Alert>
+        )}
+        <form action={formAction}>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+              />
+            </div>
+            <div className="grid gap-3">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <a
+                  href="#"
+                  className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <Input id="password" type="password" name="password" />
+            </div>
+            <div className="flex flex-col gap-3">
+              <SubmitButton />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
