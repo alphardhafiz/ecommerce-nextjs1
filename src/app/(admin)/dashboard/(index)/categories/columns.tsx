a76a1b7e8cfb8318f88862fce79fb8dc23ad53ac
buci <bucialphard@gmail.com>
2025-06-21 14:28:21 +0700

    feat(categories): integration create category done

--- src/app/(admin)/dashboard/(index)/categories/columns.tsx
+++ src/app/(admin)/dashboard/(index)/categories/columns.tsx
@@ -18,10 +18,10 @@ export const columns: ColumnDef<Category>[] = [
       return (
         <div className="space-x-4">
           <Button size="sm">
-            <Edit className="w-4 h-4" /> Edit
+            <Edit className="w-4 h-4 mr-2" /> Edit
           </Button>
           <Button variant="destructive" size="sm">
-            <Trash className="w-4 h-4" /> Delete
+            <Trash className="w-4 h-4 mr-2" /> Delete
           </Button>
         </div>
       );
--- src/app/(admin)/dashboard/(index)/categories/lib/actions.ts
+++ src/app/(admin)/dashboard/(index)/categories/lib/actions.ts
@@ -3,18 +3,31 @@
 import { schemaCategory } from "@/lib/schema";
 import { ActionResult } from "@/types";
 import { redirect } from "next/navigation";
+import prisma from "../../../../../../../lib/prisma";
 
 export async function postCategory(
   _: unknown,
   formData: FormData
 ): Promise<ActionResult> {
-  console.log(formData.get("name"));
   const validate = schemaCategory.safeParse({ name: formData.get("name") });
+
   if (!validate.success) {
     return {
       error: validate.error.errors[0].message,
     };
   }
+
+  try {
+    await prisma.category.create({
+      data: {
+        name: validate.data.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return redirect("/dasboard/categories/create");
+  }
+
   return redirect("/dashboard/categories");
 }
 
@@ -23,7 +36,6 @@ export async function updateCategory(
   formData: FormData,
   id: number | undefined
 ) {
-  console.log(formData.get("name"), id);
   const validate = schemaCategory.safeParse({ name: formData.get("name") });
   if (!validate.success) {
     return {
