f9c37259c008c3f66cc37a6f6668579c8914cddf
buci <bucialphard@gmail.com>
2025-06-21 13:30:45 +0700

    feat(categories): slicing ui & integrate get categories

---
+++ src/app/(admin)/dashboard/(index)/categories/columns.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Category } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { Edit, Trash } from "lucide-react";
+
+export const columns: ColumnDef<Category>[] = [
+  {
+    accessorKey: "name",
+    header: "Category Name",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const category = row.original;
+
+      return (
+        <div className="space-x-4">
+          <Button size="sm">
+            <Edit className="w-4 h-4" /> Edit
+          </Button>
+          <Button variant="destructive" size="sm">
+            <Trash className="w-4 h-4" /> Delete
+          </Button>
+        </div>
+      );
+    },
+  },
+];
--- src/app/(admin)/dashboard/(index)/categories/page.tsx
+++ src/app/(admin)/dashboard/(index)/categories/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { getCategories } from "./lib/data";
 
-export default function Categories() {
+export default async function CategoriesPage() {
+  const data = await getCategories()
   return (
-    <div>Categories</div>
-  )
+    <div className="space-y-4">
+      <div className="text-right">
+        <Button size="sm" className="h-8 gap-1" asChild>
+          <Link href="/dashboard/categories/create">
+            <PlusCircle className="h-3.5 w-3.5 text-accent-foreground" />
+            <span className="sr-only text-accent-foreground sm:not-sr-only sm:whitespace-nowrap">
+              Add Category
+            </span>
+          </Link>
+        </Button>
+      </div>
+      <Card x-chunk="dashboard-06-chunk-0">
+        <CardHeader>
+          <CardTitle>Categories</CardTitle>
+          <CardDescription>
+            Manage your categories and view their sales performance.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
