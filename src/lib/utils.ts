b4611721bb13d509fefdd79faf3f7b79f3874f54
buci <bucialphard@gmail.com>
2025-06-29 17:22:50 +0700

    feat(utils): new function rupiahFormat, dateFormat using dayjs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import dayjs from "dayjs";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function rupiahFormat(value: number) {
+  return Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(value);
+}
+
+export function dateFormat(date: Date | null, format = "DD MMMM YYYY") {
+  const dateToFormat = date ? dayjs(date) : dayjs();
+  return dateToFormat.format(format);
 }
