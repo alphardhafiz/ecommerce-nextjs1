bcb52c5397d6e245ce191313eeda6d5711a29881
buci <bucialphard@gmail.com>
2025-06-29 17:35:15 +0700

    feat(product): integration read product

--- src/app/(admin)/dashboard/(index)/products/page.tsx
+++ src/app/(admin)/dashboard/(index)/products/page.tsx
@@ -5,8 +5,11 @@ import { PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import { columns } from './columns'
+import { getProducts } from './lib/data'
 
-export default function ProductPage() {
+export default async function ProductPage() {
+  const data = await getProducts()
+  
   return (
      <div className="space-y-4">
       <div className="text-right">
@@ -27,7 +30,7 @@ export default function ProductPage() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <DataTable columns={columns} data={[]} />
+          <DataTable columns={columns} data={data} />
         </CardContent>
       </Card>
     </div>
