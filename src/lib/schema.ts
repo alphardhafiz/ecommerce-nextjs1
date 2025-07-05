b4f5b5440a5b422efe77fbb4e64fea493710a443
buci <bucialphard@gmail.com>
2025-07-05 14:11:01 +0700

    feat(schema): add new schema for product

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
-export const ALLOW_MIME_TYPES = ["image/jpg", "image/jpeg", "image/png"];
+export const ALLOW_MIME_TYPES = [
+  "image/jpg",
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+];
 
 export const schemaSignIn = z.object({
   email: z
@@ -27,4 +32,37 @@ export const schemaBrand = schemaCategory.extend({
       message: "Only .jpg, .png, .webp formats are supported",
     })
     .refine((file) => file?.name, { message: "Image is required" }),
+});
+
+export const schemaProduct = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(4, { message: "Name should have min 4 characters" }),
+  description: z
+    .string({ required_error: "Description is required" })
+    .min(10, { message: "Description should have min 10 characters" }),
+  price: z.string({ required_error: "Price is required" }),
+  stock: z.string({ required_error: "Stock is required" }),
+  brand_id: z.string({ required_error: "Brand is required" }),
+  category_id: z.string({ required_error: "Category is required" }),
+  location_id: z.string({ required_error: "Location is required" }),
+  images: z
+    .any()
+    .refine((files: File[]) => files.length === 3, {
+      message: "Please upload 3 images of the product",
+    })
+    .refine(
+      (files: File[]) => {
+        let validate = false;
+
+        Array.from(files).find((file) => {
+          validate = ALLOW_MIME_TYPES.includes(file.type);
+        });
+
+        return validate;
+      },
+      {
+        message: "Only .jpg, .png, .webp formats are supported",
+      }
+    ),
 });
