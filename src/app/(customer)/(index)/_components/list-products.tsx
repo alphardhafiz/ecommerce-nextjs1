39e83ca50335e2060611c0ac2906a120f331faa2
alphardhafiz <bucialphard@gmail.com>
2025-08-16 20:13:49 +0700

    feat(catalogs): slicing ui catalog page done

--- src/app/(customer)/(index)/_components/list-products.tsx
+++ src/app/(customer)/(index)/_components/list-products.tsx
@@ -3,10 +3,11 @@ import { getProducts } from "../lib/data";
 import Link from "next/link";
 import Image from "next/image";
 import { rupiahFormat } from "@/lib/utils";
+import CardProduct from "./card-product";
 
 interface ListProductsProps {
   title: ReactNode;
-  type: 'best-selling' | 'latest'
+  type: "best-selling" | "latest";
 }
 
 export default async function ListProducs({ title, type }: ListProductsProps) {
@@ -25,33 +26,16 @@ export default async function ListProducs({ title, type }: ListProductsProps) {
       </div>
       <div className="grid grid-cols-5 gap-[30px]">
         {products.map((item) => (
-          <Link
+          <CardProduct
             key={`${item.name + item.id}`}
-            href="#"
-            className="product-card"
-          >
-            <div className="bg-white flex flex-col gap-[24px] p-5 rounded-[20px] ring-1 ring-[#E5E5E5] hover:ring-2 hover:ring-[#FFC736] transition-all duration-300 w-full">
-              <div className="relative w-full h-[90px] flex shrink-0 items-center justify-center overflow-hidden">
-                <Image
-                  src={item.image_url}
-                  fill
-                  className="object-contain"
-                  alt={item.name}
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <div className="flex flex-col gap-1">
-                  <p className="font-semibold leading-[22px]">
-                    {item.name}
-                  </p>
-                  <p className="text-sm text-[#616369]">{item.category.name}</p>
-                </div>
-                <p className="font-semibold text-[#0D5CD7] leading-[22px]">
-                  {rupiahFormat(Number(item.price))}
-                </p>
-              </div>
-            </div>
-          </Link>
+            item={{
+              category_name: item.category.name,
+              id: item.id,
+              image_url: item.image_url,
+              name: item.name,
+              price: Number(item.price),
+            }}
+          />
         ))}
       </div>
     </div>
---
+++ src/app/(customer)/(index)/catalogs/_components/product-listing.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import CardProduct from "../../_components/card-product";
+
+export default function ProductListing() {
+  return (
+    <div className="grid grid-cols-3 gap-[30px]">
+      <CardProduct
+        item={{
+          category_name: "Desktops",
+          id: 1,
+          image_url:
+            "assets/thumbnails/color_back_green__buxxfjccqjzm_large_2x-Photoroom 1.png",
+          name: "iMac Green Energy",
+          price: 24000000,
+        }}
+      />
+    </div>
+  );
+}
